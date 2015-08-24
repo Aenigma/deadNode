c4563d35b6d18532b028136e29fac757bf1b366c
Kevin Raoofi <nullindev@gmail.com>
2015-08-24 18:58:22 -0400

    Made all IRC client options exposed via config.json
    Removed some useless console.log stuff from CommandEmitter.js
    
    Removed hardcoded config stuff in index.js

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ var config = require('./config/config.json');
 
 var PLUGIN_DIR = path.join(__dirname, "lib/plugins/");
 
-var client = new irc.Client(config.server, config.nick,  {
-  debug:true,
-  channels: config.channels
-});
+var client = new irc.Client(config.server, config.nick, config.options);
 
 console.log(PLUGIN_DIR);
 fs.readdir(PLUGIN_DIR, function(err, files) {
@@ -17,20 +14,13 @@ fs.readdir(PLUGIN_DIR, function(err, files) {
   .map(function(file) {
     var plugin = require(path.join(PLUGIN_DIR, file));
 
-    console.log('loading: ' + file);
-
     plugin(client);
   });
 });
-client.on('message', function(from, to, message, text) {
-  //client.emit('testevent', "this is a test!");
-  console.log(from + ' => ' + to + ': ' + message);
-  //console.log(text);
-});
 
-client.on('testevent', function(arg) {
-  console.log(arg);
-})
+client.on('message', function(from, to, message, text) {
+  console.log(from + ' => ' + to + ': ' + message);
+});
 
 client.on('error', function(message) {
     console.error(message);
